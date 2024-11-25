f4dc862edcecc0254dd8e1db6ca6eee706e4aed0
Henio Junior <henioajunior@gmail.com>
2024-11-25 07:36:01 -0300

    Create User entity and H2 DB

---
+++ src/main/java/com/heniojunior/dscommerce/entities/User.java
@@ -0,0 +1,54 @@
+package com.heniojunior.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private LocalDate birthDate;
+    private String password;
+
+    public User() {}
+
+    public User(Long id, String name, String email, String phone, LocalDate birthDate, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.birthDate = birthDate;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
