214b4bdec8f84c409ac07cdbe6b4c709d1edb902
Henio Junior <henioajunior@gmail.com>
2024-11-28 11:49:17 -0300

    Created getOrders and getProducts method

--- src/main/java/com/heniojunior/dscommerce/entities/Product.java
+++ src/main/java/com/heniojunior/dscommerce/entities/Product.java
@@ -3,6 +3,7 @@ package com.heniojunior.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -64,5 +65,13 @@ public class Product {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Order> getOrders() {
+        return items.stream().map(x -> x.getOrder()).toList();
     }
 }
