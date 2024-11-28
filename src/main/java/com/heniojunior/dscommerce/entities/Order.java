59cac7092b9f6e0ec39bffe5cbb1df004f3de8a2
Henio Junior <henioajunior@gmail.com>
2024-11-28 07:52:04 -0300

    Created Payment

--- src/main/java/com/heniojunior/dscommerce/entities/Order.java
+++ src/main/java/com/heniojunior/dscommerce/entities/Order.java
@@ -11,21 +11,27 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant moment;
     private OrderStatus status;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
     public Order() {
     }
 
-    public Order(Long id, Instant moment, OrderStatus status, User client) {
+    public Order(Long id, Instant moment, OrderStatus status, User client, Payment payment) {
         this.id = id;
         this.moment = moment;
         this.status = status;
         this.client = client;
+        this.payment = payment;
     }
 
     public Long getId() {
@@ -42,5 +48,9 @@ public class Order {
 
     public User getClient() {
         return client;
+    }
+
+    public Payment getPayment() {
+        return payment;
     }
 }
